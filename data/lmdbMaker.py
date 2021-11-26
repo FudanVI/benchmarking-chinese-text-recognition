27ea0ef55326212759b54000681a3e6a1814caa1
JingyeChen <576194329@qq.com>
2021-11-26 12:58:03 +0800

    add lmdbMaker/lmdbReader

---
+++ data/lmdbMaker.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+Please execute the code with python2
+'''
+import os
+import lmdb
+import cv2
+import numpy as np
+
+def checkImageIsValid(imageBin):
+    if imageBin is None:
+        return False
+    try:
+        imageBuf = np.fromstring(imageBin, dtype=np.uint8)
+        img = cv2.imdecode(imageBuf, cv2.IMREAD_GRAYSCALE)
+        imgH, imgW = img.shape[0], img.shape[1]
+        if imgH * imgW == 0:
+            return False
+    except:
+        print("Image is invalid!")
+        return False
+    return True
+
+
+def writeCache(env, cache):
+    with env.begin(write=True) as txn:
+        for k, v in cache.items():
+            txn.put(k, v)
+
+
+def createDataset(outputPath, imagePathList, labelList, lexiconList=None, checkValid=True):
+    """
+    ARGS:
+        outputPath    : LMDB output path
+        imagePathList : list of image path
+        labelList     : list of corresponding groundtruth texts
+        lexiconList   : (optional) list of lexicon lists
+        checkValid    : if true, check the validity of every image
+    """
+
+    assert (len(imagePathList) == len(labelList))
+
+    nSamples = len(imagePathList)
+
+    env = lmdb.open(outputPath, map_size=1099511627776)
+    cache = {}
+    cnt = 1
+    for i in range(nSamples):
+        imagePath = imagePathList[i]
+        label = labelList[i]
+
+        if not os.path.exists(imagePath):
+            print('%s does not exist' % imagePath)
+            continue
+
+        import codecs
+        with open(imagePath, 'r') as f:
+            imageBin = f.read()
+        if checkValid:
+            if not checkImageIsValid(imageBin):
+                print('%s is not a valid image' % imagePath)
+                continue
+
+        imageKey = 'image-%09d' % cnt
+        labelKey = 'label-%09d' % cnt
+        cache[imageKey] = imageBin
+
+        cache[labelKey] = label
+
+        if lexiconList:
+            lexiconKey = 'lexicon-%09d' % cnt
+            cache[lexiconKey] = ' '.join(lexiconList[i])
+        if cnt % 1000 == 0:
+            writeCache(env, cache)
+            cache = {}
+            print('Written %d / %d' % (cnt, nSamples))
+        cnt += 1
+    nSamples = cnt - 1
+    cache['num-samples'] = str(nSamples)
+    writeCache(env, cache)
+    print('Created dataset with %d samples' % nSamples)
+
+
+def get_mydataset():
+    lines = open('.../label.txt', 'r').readlines()
+    image_list = []
+    label_list = []
+    for line in lines:
+        line = line.strip()
+        image, label = line.split()
+        image_list.append(image)
+        label_list.append(label)
+
+    return image_list, label_list
+
+
+if __name__ == '__main__':
+    imgList, labelList = get_scut_train()
+    print("The length of the list is ", len(imgList))
+
+    '''Input the address you want to generate the lmdb file.'''
+    createDataset('./root_to_generate'.format(), imgList, labelList)
+
---
+++ data/lmdbReader.py
@@ -0,0 +1,84 @@
+import six
+import sys
+import lmdb
+import torch
+import random
+import numpy as np
+from PIL import Image
+from torch.utils.data import Dataset
+from torch.utils.data import sampler
+import torchvision.transforms as transforms
+
+class lmdbDataset(Dataset):
+    def __init__(self, root=None, transform=None):
+
+        self.env = lmdb.open(
+            root,
+            max_readers=1,
+            readonly=True,
+            lock=False,
+            readahead=False,
+            meminit=False)
+
+        if not self.env:
+            print('cannot creat lmdb from %s' % (root))
+            sys.exit(0)
+
+        with self.env.begin(write=False) as txn:
+            nSamples = int(txn.get('num-samples'.encode()))
+            self.nSamples = nSamples
+
+        self.transform = transform
+
+    def __len__(self):
+        return self.nSamples
+
+    def __getitem__(self, index):
+        if index > len(self):
+            index = len(self) - 1
+        assert index <= len(self), 'Error %d' % index
+        index += 1
+        with self.env.begin(write=False) as txn:
+            img_key = 'image-%09d' % index
+            imgbuf = txn.get(img_key.encode())
+
+            buf = six.BytesIO()
+            buf.write(imgbuf)
+            buf.seek(0)
+            try:
+                img = Image.open(buf)
+            except IOError:
+                print('Corrupted image for %d' % index)
+                return self[index + 1]
+
+            label_key = 'label-%09d' % index
+            label = str(txn.get(label_key.encode()).decode('utf-8'))
+
+            if len(label) <= 0:
+                return self[index + 1]
+
+            label = label.lower()
+
+            if self.transform is not None:
+                img = self.transform(img)
+
+        return (img, label)
+
+class resizeNormalize(object):
+
+    def __init__(self, size, interpolation=Image.BILINEAR):
+
+        self.size = size
+        self.interpolation = interpolation
+        self.toTensor = transforms.ToTensor()
+
+    def __call__(self, img):
+
+        img = img.resize(self.size, self.interpolation)
+        img = self.toTensor(img)
+        img.sub_(0.5).div_(0.5)
+        return img
+
+if __name__ == '__main__':
+    dataset = lmdbDataset('root of dataset')
+    print(f'Length of dataset {len(dataset)}')

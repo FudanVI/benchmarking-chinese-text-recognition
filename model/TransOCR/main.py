a55a483cfac21bb57e61bbe5ac44ea350bb9e949
HaiyangYu <20210240072@fudan.edu.cn>
2022-06-15 16:39:38 +0800

    modify the files of transocr

--- model/TransOCR/main.py
+++ model/TransOCR/main.py
@@ -9,7 +9,7 @@ import zhconv
 
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('--exp_name', type=str, default='test', help='')
-parser.add_argument('--batch_size', type=int, default=64, help='')
+parser.add_argument('--batch_size', type=int, default=32, help='')
 parser.add_argument('--lr', type=float, default=1.0, help='')
 parser.add_argument('--epoch', type=int, default=1000, help='')
 parser.add_argument('--radical', action='store_true', default=False)
@@ -149,7 +149,8 @@ def test(epoch):
 
 if __name__ == '__main__':
     print('-------------')
-    os.mkdir('./history/{}'.format(args.exp_name))
+    if not os.path.isdir('./history/{}'.format(args.exp_name)):
+        os.mkdir('./history/{}'.format(args.exp_name))
     if args.test:
         test(-1)
         exit(0)
